5ab22a23a1d0f032cfc46c05adeaa4265d98b303
TheSCodex <eliud1102@outlook.es>
2023-11-05 00:02:34 -0500

    recetas e ingredientes full

--- src/routes/ingredientesRoutes.js
+++ src/routes/ingredientesRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 const ingredientsRoutes = express.Router();
 import * as ingredientsControllers from '../controllers/ingredientesControllers.js'
 
-ingredientsRoutes.post('/recetas/:id/ingredientes', ingredientsControllers.createIngredient);
-ingredientsRoutes.get('/recetas/:id/ingredientes', ingredientsControllers.getIngredients);
-ingredientsRoutes.get('/recetas/:id/ingredientes/:id', ingredientsControllers.getIngredientsById);
+ingredientsRoutes.post('/ingredientes', ingredientsControllers.createIngredient);
+ingredientsRoutes.get('/ingredientes', ingredientsControllers.getIngredients);
+ingredientsRoutes.get('/ingredientes/:id', ingredientsControllers.getIngredientsById);
+ingredientsRoutes.get('/recetas/:id/ingredientes', ingredientsControllers.getIngredientsByRecipe);
 ingredientsRoutes.put('/recetas/:id/ingredientes/:id', ingredientsControllers.updateIngredient);
-ingredientsRoutes.delete('/recetas/:id/ingredientes/:id', ingredientsControllers.deleteIngredient);
+ingredientsRoutes.delete('/ingredientes/:id', ingredientsControllers.deleteIngredient);
 
 export default ingredientsRoutes;
